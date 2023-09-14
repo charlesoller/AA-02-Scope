30473395251b60966b19054ae2a8b0d7dcd00331
charlesoller <charlesrello@gmail.com>
2023-09-14 19:32:31 -0400

    11 Done

--- problems/10-count-down.js
+++ problems/10-count-down.js
@@ -28,8 +28,35 @@ Example 4:
 
 ***********************************************************************/
 
-// Your code here 
+const countDownTimer = (n) => {
+  if (n === 0){return "Happy New Year"};
+  return cb = () => {
+    n--;
+    if (n <= 0){
+      return "Happy New Year";
+    } else {
+      return cb;
+    }
+  }
+}
 
+//Example 1:
+  console.log(countDownTimer(0)); // prints "Happy New Year!"
+
+//Example 2:
+  let oneDay = countDownTimer(1); // returns a function
+  console.log(oneDay()); // prints "Happy New Year!"
+
+//Example 3:
+  let twoDays = countDownTimer(2); // returns a function
+  console.log(twoDays()); // returns a function
+  console.log(twoDays()); // prints "Happy New Year!"
+
+//Example 4:
+  let threeDays = countDownTimer(3); // returns a function
+  console.log(threeDays()); // returns a function
+  console.log(threeDays()); // returns a function
+  console.log(threeDays()); // prints "Happy New Year!"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -22,7 +22,25 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   fat-arrow function
 ***********************************************************************/
 
-// Your code here 
+// const lazyAdder = num1 => {
+//   return num2 => {
+//     return num3 => {
+//       return num1 + num2 + num3;
+//     }
+//   }
+// }
+
+const lazyAdder = num1 => num2 => num3 => num1 + num2 + num3;
+
+let firstAdd = lazyAdder(1);
+let secondAdd = firstAdd(2);
+let sum = secondAdd(3);
+console.log(sum); // prints 6
+
+let func1 = lazyAdder(10);
+let func2 = func1(20);
+let total = func2(3);
+console.log(total); // prints 33
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
