4b2dd89e6bc35e6f6b169867def34d9cd314c499
charlesoller <charlesrello@gmail.com>
2023-09-14 21:08:46 -0400

    14 done

--- problems/13-dynamic-divide.js
+++ problems/13-dynamic-divide.js
@@ -18,8 +18,19 @@ divideByFive(50); // returns 10
 
 ***********************************************************************/
 
-// Your code here 
+const dynamicDivide = (divisor) => {
+  return dividend = (num) => num / divisor;
+}
 
+
+const halfer = dynamicDivide(2); // returns a function
+console.log(halfer(20)); // returns 10
+
+const divideByThree = dynamicDivide(3);
+divideByThree(30); // returns 10
+
+const  divideByFive = dynamicDivide(5);
+divideByFive(50); // returns 10
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -22,7 +22,52 @@ console.log(smoothie2("pineapple"));
 // prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 ***********************************************************************/
 
-// Your code here 
+const smoothieMachine = (...args1) => {
+  let order = ""
+  return inner = (...args2) => {
+
+    if (typeof args1 != 'string' && args1.length > 0){
+      args1 = args1.join(" and ");
+    }
+    if (typeof args2 != 'string' && args2.length > 0){
+      args2 = args2.join(" and ");
+    }
+
+    //case that no initial values are passed
+    if(order.length === 0 && args1.length === 0){
+      order += "I'm having a smoothie with ";
+      order += args2;
+    } else if (args1.length === 0){
+      order += " and " + args2;
+    }
+
+    //case that initial values are passed
+    if(order.length === 0 && args1.length !== 0){
+      order += "I'm having a smoothie with ";
+      order += args1;
+      if (args2.length !== 0){
+        order += " and " + args2;
+      } else {
+        order += " and " + args2;
+      }
+    }
+
+    return order;
+  }
+}
+
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+//prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+let smoothie2 = smoothieMachine("apples", "bananas", "berries");
+console.log(smoothie2("pineapple"));
+// prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
